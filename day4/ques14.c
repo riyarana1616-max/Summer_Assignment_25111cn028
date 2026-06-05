b45427037bfb0c4eb2e46a3936d91a85445a2acc
Riya Rana <riyarana1616@gmail.com>
2026-06-05 13:49:56 +0530

    day4 c programs

---
+++ day4/ques14.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,a=0,b=1,c,i;
+    printf("enter n:");
+    scanf("%d",&n);
+
+    if(n==1)
+      printf("Nth Fibonacci term =%d",a);
+    else if (n==2)
+      printf("Nth Fibonacci term =%d",b);
+    else
+    {
+        for(i=3;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        printf("Nth Fibonacci term =%d",c);
+    }
+    return 0;
+}
